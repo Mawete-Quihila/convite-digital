940038aed354ade5466d4f9cabf7a97f2b8c9639
Mawete Quihila <mawe.quihila@gmail.com>
2025-04-06 18:44:36 +0100

    Novo Commit atualizando o serverJs

--- server/server.js
+++ server/server.js
@@ -1,40 +1,3 @@
-/*const express = require('express');
-const path = require('path');
-const { initializeDatabase } = require('./database');
-const routes = require('./routes');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middlewares
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Rotas
-app.use('/api', routes);
-
-// Rota fallback para SPA
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-// Inicialização
-initializeDatabase();
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-router.delete('/reset', async (req, res) => {
-    try {
-        db.run("DELETE FROM reservas", (err) => {
-            if (err) throw err;
-            res.json({ success: true, message: "Todas as reservas foram apagadas!" });
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Erro ao resetar o banco de dados" });
-    }
-});*/
-
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -57,7 +20,9 @@ router.delete('/reset', async (req, res) => {
 });
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://convite-digital-front.onrender.com'
+  }));
 const PORT = process.env.PORT || 3000;
 
 // Configurações
